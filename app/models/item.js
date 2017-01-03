0a0c8507d635b166334ca190efcddc26bafe8423
Ryan Tablada <ryan.tablada@gmail.com>
2017-01-03 11:29:05 -0600

    refactor: change rooms to be model based

--- app/models/item.js
+++ app/models/item.js
@@ -2,7 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   trip: DS.belongsTo('trip'),
+  room: DS.belongsTo('room'),
   name: DS.attr('string'),
   ownBox: DS.attr('boolean'),
-  room: DS.attr('string')
+  roomName: DS.attr('string')
 });
